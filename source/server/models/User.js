ca711d21aeadefe7ab758b018425ead0295b487f
fiher <fihercho@gmail.com>
2017-07-07 21:51:26 +0300

    User model done

---
+++ source/server/models/User.js
@@ -0,0 +1,19 @@
+/**
+ * Created by PC on 07-Jul-17.
+ */
+const mongoose = require('mongoose')
+// TODO: ObjectID might be with lower D
+
+let userSchema = mongoose.Schema({
+  username: {type: mongoose.Schema.Types.String, required: true},
+  profilePicture: {type: mongoose.Schema.Types.String, required: false},
+  posts: {type: mongoose.Schema.Types.ObjectID, ref: 'Post'},
+  comments: {type: mongoose.Schema.Types.ObjectID, ref: 'Comment'},
+  upvotedPosts: [{type: mongoose.Schema.Types.ObjectID, ref: 'Post'}],
+  downvotedPosts: [{type: mongoose.Schema.Types.ObjectID, ref: 'Post'}],
+  salt: {type: mongoose.Schema.Types.String, required: true},
+  hashedPassword: {type: mongoose.Schema.Types.String, required: true},
+  roles: [{type: mongoose.Schema.Types.String, required: true}]
+})
+const User = mongoose.model('User', userSchema)
+module.exports = User
