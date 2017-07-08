9ea8383b93dc01731f67b297c3f543db895fc778
Silvave <todor.vasilev@yahoo.com>
2017-07-08 11:34:45 +0300

    Added client folder to be done

---
+++ source/server/server.js
@@ -0,0 +1,17 @@
+const env = require('./config/environment')
+const expressConfig = require('./config/express')
+
+const app = require('express')()
+
+module.exports = () => {
+  // Setup MongoDB connection
+  require('./config/database')(env)
+
+  // Initialize passport
+  require('./config/passport')()
+
+  // Import server-routes.
+  require('./config/server-routes')(app)
+}
+
+
