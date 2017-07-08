31208abd60425f9ce0dcdeaf2f4eedc3d612990a
fiher <fihercho@gmail.com>
2017-07-09 00:39:40 +0300

    Comment controller ready. Now we can get and post comments

--- source/server/controllers/comment.js
+++ source/server/controllers/comment.js
@@ -3,10 +3,29 @@ const Comment = require('../models/Comment')
 module.exports = {
   getMemeComments: {
     get: (req, res) => {
+      let memeId = req.params.memeId
 
+      Comment.find({meme: memeId})
+        .sort({createdOn: -1})
+        .populate('author', 'replies')
+        .then(comments => {
+          res.status(200).send(comments)
+        })
     },
     post: (req, res) => {
+      let userId = req.user._id
+      let memeId = req.params.memeId
+      let commentData = {
+        author: userId,
+        meme: memeId,
+        content: req.body.content,
+        replies: [],
+        upvotes: []
+      }
 
+      Comment.create(commentData).then(comment => {
+        res.status(200).send({comment})
+      })
     }
   }
 }
