9cb3e389da8e8d8d9c0b49dc954323000c0ee360
fiher <fihercho@gmail.com>
2017-07-09 04:22:58 +0300

    Cypher4 ready

--- source/server/utilities/a-cript/encrypt/Encryption.js
+++ source/server/utilities/a-cript/encrypt/Encryption.js
@@ -48,7 +48,23 @@ export default class Decryption {
   }
 
   cypher4 (text, publicKey, privateKey, secretKey) {
-    return text
+    let muttableText = text.split()
+    let textLength = text.length
+    let publicKeyLength = publicKey.length
+    let publicKeyIndex = 0
+    let privateKeyLength = privateKey.length
+    let privateKeyIndex = 0
+    let secretKeyLength = secretKey.length
+    let secretKeyIndex = 0
+    for (let i = 0; i < textLength; i++) {
+      if (publicKeyIndex === publicKeyLength - 1) { publicKeyIndex = 0 }
+      if (privateKeyIndex === privateKeyLength - 1) { privateKeyIndex = 0 }
+      if (secretKeyIndex === secretKeyLength - 1) { secretKeyIndex = 0 }
+      let letterASCII = text.charCodeAt(i)
+      let newASCII = publicKey.charCodeAt(publicKeyIndex) + privateKey.charCodeAt(privateKeyIndex) + secretKey.charCodeAt(secretKeyIndex)
+      muttableText[i] = String.fromCharCode(letterASCII + newASCII)
+    }
+    return muttableText.join('')
   }
 
   cypher5 (text) {
--- source/server/utilities/a-cript/encrypt/test-encryption.js
+++ source/server/utilities/a-cript/encrypt/test-encryption.js
@@ -1,16 +1,24 @@
 /**
  * Created by PC on 09-Jul-17.
  */
-function cypher5 (text) {
+function cypher4 (text, publicKey, privateKey, secretKey) {
   let muttableText = text.split()
   let textLength = text.length
+  let publicKeyLength = publicKey.length
+  let publicKeyIndex = 0
+  let privateKeyLength = privateKey.length
+  let privateKeyIndex = 0
+  let secretKeyLength = secretKey.length
+  let secretKeyIndex = 0
   for (let i = 0; i < textLength; i++) {
-    let asciiChange = i + 1
+    if (publicKeyIndex === publicKeyLength - 1) { publicKeyIndex = 0 }
+    if (privateKeyIndex === privateKeyLength - 1) { privateKeyIndex = 0 }
+    if (secretKeyIndex === secretKeyLength - 1) { secretKeyIndex = 0 }
     let letterASCII = text.charCodeAt(i)
-    letterASCII += asciiChange
-    muttableText[i] = String.fromCharCode(letterASCII)
+    let newASCII = publicKey.charCodeAt(publicKeyIndex) + privateKey.charCodeAt(privateKeyIndex) + secretKey.charCodeAt(secretKeyIndex)
+    muttableText[i] = String.fromCharCode(letterASCII + newASCII)
   }
   return muttableText.join('')
 }
-let a = cypher5('pesho')
+let a = cypher4('pesho', 'misho', 'mamaMia', 'oleole')
 console.log(a)
