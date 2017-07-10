86bc83f1c65ee05863a62cc14e7b05c43b417449
fiher <fihercho@gmail.com>
2017-07-10 15:04:04 +0300

    MemeInfo componenet. Fixed Style errors on other componenets

--- source/client/components/AllAuthors.js
+++ source/client/components/AllAuthors.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 export default class AllAuthors extends Component {
-  render() {
+  render () {
     return (
       <div>
         Hi from AllAuthors!
--- source/client/components/MemeInfo.js
+++ source/client/components/MemeInfo.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
 
 export default class AllMemes extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      title: props.title,
+      author: props.author,
+      link: props.memeUrl,
+      createdOn: props.createdOn,
+      comments: props.comments,
+      upVotedUsers: props.upVotes,
+      downVotedUsers: props.downVotes,
+      upVotesCount: props.upVotesCount,
+      downVotesCount: props.downVotesCount
+    }
+  }
+
+  getUpvotesCount () {
+    let upvotes = this.state.upVotesCount - this.state.downVotesCount
+    return upvotes > 0 ? upvotes : 0
+  }
+
+  render () {
     return (
       <div>
+        <div>
+          {this.state.title}
+        </div>
+        <div>
+          {this.state.createdOn}
+        </div>
+        <div>
+          <img src={this.state.link}/>
+        </div>
+        <div>
+          {this.getUpvotesCount()}
+        </div>
         Hi from MemeInfo!
       </div>
     )
