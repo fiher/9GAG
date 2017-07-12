c4fe638afdbe3b032edcca752e04921b68f7d59f
Silvave <todor.vasilev@yahoo.com>
2017-07-12 04:27:22 +0300

    Changed user model

--- source/server/models/User.js
+++ source/server/models/User.js
@@ -13,31 +13,15 @@ let userSchema = mongoose.Schema({
   },
   salt: {type: mongoose.Schema.Types.String, required: true},
   profilePicture: {type: mongoose.Schema.Types.String},
-  firstName: {
-    type: mongoose.Schema.Types.String,
-    required: requiredMsg('First name')
-  },
-  lastName: {
-    type: mongoose.Schema.Types.String,
-    required: requiredMsg('Last name')
-  },
-  age: {
-    type: mongoose.Schema.Types.Number,
-    min: [0, 'Age must be between 0 and 120'],
-    max: [120, 'Age must be between 0 and 120']
-  },
-  gender: {
-    type: mongoose.Schema.Types.String,
-    enum: {
-      values: ['Male', 'Female', 'Other'],
-      message: 'Gender should be either "Male", "Female" or "Other"'
-    }
-  },
+  firstName: { type: mongoose.Schema.Types.String },
+  lastName: { type: mongoose.Schema.Types.String },
+  age: { type: mongoose.Schema.Types.Number },
+  gender: { type: mongoose.Schema.Types.String },
   memes: {type: mongoose.Schema.Types.ObjectId, ref: 'Meme'},
   comments: {type: mongoose.Schema.Types.ObjectId, ref: 'Comment'},
   upVotedMemes: [{type: mongoose.Schema.Types.ObjectId, ref: 'Meme'}],
   downVotedMemes: [{type: mongoose.Schema.Types.ObjectId, ref: 'Meme'}],
-  roles: [{type: mongoose.Schema.Types.String, required: true}]
+  roles: [{type: mongoose.Schema.Types.String}]
 })
 
 const User = mongoose.model('User', userSchema)
