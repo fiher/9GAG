51f19c489443ada520a134bc74e13a87aad355a3
Silvave <todor.vasilev@yahoo.com>
2017-07-12 04:28:14 +0300

    Added user state in App.js

--- source/client/components/App.js
+++ source/client/components/App.js
@@ -3,7 +3,29 @@ import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
+import UserStore from '../stores/UserStore'
+
 export default class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = UserStore.getState()
+
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange (state) {
+    this.setState(state)
+  }
+
+  componentDidMount () {
+    UserStore.listen(this.onChange)
+  }
+
+  componentWillUnmount () {
+    UserStore.unlisten(this.onChange)
+  }
+
   render () {
     return (
       <div>
