1126a252eb6f0e78e20794b947a2d7c5dccff465
Silvave <todor.vasilev@yahoo.com>
2017-07-12 03:48:19 +0300

    Added new nav bar with user menu sub component

--- source/client/components/Navbar.js
+++ source/client/components/Navbar.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+import NavbarUserMenu from './sub-components/NavbarUserMenu'
+
 export default class Navbar extends React.Component {
   render () {
+    let navbarUserMenu = <NavbarUserMenu userData={this.props.userData}/>
+
     return (
-      <nav className="navbar">
-        <ul className="nav">
-          <li className="nav-item">
-            <Link to='/'>Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/memes/all'>All Memes</Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/users/all'>All Authors</Link>
-          </li>
-          <span className="message">{this.props.greetings}</span>
-        </ul>
+      <nav className="navbar navbar-default navbar-static-top">
+        <div id="navbar" className="navbar-collapse collapse">
+          <ul className="nav navbar-nav">
+            <li className="nav-item">
+              <Link to='/'>Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/memes/all'>All Memes</Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/users/all'>All Authors</Link>
+            </li>
+          </ul>
+          {navbarUserMenu}
+        </div>
       </nav>
     )
   }
